ab36ce89d3f276d27251df284f3ea72041eb6507
PayneW <2416451355@qq.com>
2019-08-09 18:34:55 +0800

    mac edit: 20190809 01

--- Dart_语言中文教程/Dart-语法预览/function-demo.dart
+++ Dart_语言中文教程/Dart-语法预览/function-demo.dart
@@ -1,5 +1,4 @@
-/// 返回一个函数，该函数会添加 "addBy" 到
-/// 这个函数的参数上并返回
+// 返回一个函数，该函数会添加 "addBy" 到这个函数的参数上并返回
 Function makeAdder(num addBy) {
   return (num i) => addBy + i;
 }
@@ -9,7 +8,6 @@ void main() {
   var add2 = makeAdder(2);
   // 创建一个加4的函数
   var add4 = makeAdder(4);
-
 
   assert(add2(3) == 5);
   print(add2(3));
